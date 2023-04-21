7c67e7c9bf40d243cc965c41be5b1d3e33e8a040
amoledwatchfaces <92080649+amoledwatchfaces@users.noreply.github.com>
2023-04-21 11:39:42 +0200

    * moved to gradle.kts #23

--- mobile/build.gradle.kts
+++ mobile/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.weartools.weekdayutccomp"
         minSdk = 23
         targetSdk = 33
-        versionCode = 208
-        versionName = "2.0.8"
+        versionCode = 210
+        versionName = "2.1.0"
 
     }
 
--- wear/build.gradle.kts
+++ wear/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.weartools.weekdayutccomp"
         minSdk = 27
         targetSdk = 33
-        versionCode = 209
-        versionName = "2.0.9"
+        versionCode = 211
+        versionName = "2.1.1"
         resourceConfigurations += listOf("en", "cs", "de", "el", "es", "it", "pt", "ro", "sk", "zh")
     }
     bundle {
