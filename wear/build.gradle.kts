b39b29eaa673a597d2e7adef2069eb12ecfd915f
amoledwatchfaces <92080649+amoledwatchfaces@users.noreply.github.com>
2023-08-04 13:29:59 +0200

    * translations - final

--- wear/build.gradle.kts
+++ wear/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.weartools.weekdayutccomp"
         minSdk = 27
         targetSdk = 33
-        versionCode = 10000258
-        versionName = "2.5.8"
+        versionCode = 10000259
+        versionName = "2.5.9"
         resourceConfigurations += listOf("en", "cs", "de", "el", "es", "it", "pt", "ro", "sk", "zh")
     }
     bundle {
