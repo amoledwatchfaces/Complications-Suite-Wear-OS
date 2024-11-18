47741716437ce5ea2a5d7b2ef790da032f924933
amoledwatchfaces <92080649+amoledwatchfaces@users.noreply.github.com>
2024-11-18 13:43:06 +0100

    * protolayout update

--- wear/build.gradle.kts
+++ wear/build.gradle.kts
@@ -141,6 +141,10 @@ dependencies {
     // Firebase
     implementation (platform("com.google.firebase:firebase-bom:33.6.0"))
     implementation ("com.google.firebase:firebase-crashlytics")
+
+    // Protolayout
+    implementation("androidx.wear.protolayout:protolayout-expression:1.2.1")
+    implementation("androidx.wear.protolayout:protolayout:1.2.1")
 }
 
 
