6379d4121bfa649c94e299a4d93cd5a6602e99a8
amoledwatchfaces <92080649+amoledwatchfaces@users.noreply.github.com>
2024-11-18 14:20:47 +0100

    * custom goal complication update 3

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,8 +14,8 @@ tasks.register("clean", Delete::class) {
 buildscript {
 
     /** Set version for wear & mobile modules **/
-    val versionCode by extra(10000370)
-    val versionName by extra("3.7.0")
+    val versionCode by extra(10000371)
+    val versionName by extra("3.7.1")
 
     dependencies {
         classpath ("com.android.tools.build:gradle:8.6.1")
