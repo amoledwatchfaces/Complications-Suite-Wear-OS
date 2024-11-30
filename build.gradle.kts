b9eff49ac4971d1389552b98d70ef01f3cd321c9
amoledwatchfaces <92080649+amoledwatchfaces@users.noreply.github.com>
2024-11-30 17:31:48 +0100

    * world clock fixes

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,8 +14,8 @@ tasks.register("clean", Delete::class) {
 buildscript {
 
     /** Set version for wear & mobile modules **/
-    val versionCode by extra(10000381)
-    val versionName by extra("3.8.1")
+    val versionCode by extra(10000382)
+    val versionName by extra("3.8.2")
 
     dependencies {
         classpath ("com.android.tools.build:gradle:8.6.1")
